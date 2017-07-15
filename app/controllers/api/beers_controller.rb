726fcb8a28decd205b760128e22f605f7986dffa
jennafrancis <jef399@nyu.edu>
2017-07-15 10:46:37 -0400

    Create beer method working

--- app/controllers/api/beers_controller.rb
+++ app/controllers/api/beers_controller.rb
@@ -3,4 +3,18 @@ class Api::BeersController < ApplicationController
     @beers = Beer.all
     render json: @beers
   end
+
+  def create
+    @beer = Beer.new(beer_params)
+    if @beer.save
+      render json: @beer
+    else
+      render json: { errors: { message: "This beer failed to save."}}
+    end
+  end
+
+  private
+    def beer_params
+      params.require(:beer).permit(:name, :brewery, :ibu, :abv, :description, :image_url, :style)
+    end
 end
