08842fff3bc0116371abc377ebce6851e6131881
jennafrancis <jef399@nyu.edu>
2017-07-17 15:10:43 -0400

    add dependent: :destroy to appropriate models

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,6 +1,6 @@
 class Wishlist < ApplicationRecord
   belongs_to :user
-  has_many :wishlist_beers
+  has_many :wishlist_beers, dependent: :destroy
   has_many :beers, through: :wishlist_beers
 
   validates_presence_of :user_id
