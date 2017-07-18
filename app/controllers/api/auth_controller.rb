b9353ecb3cc5f96d2e08dbb189219caffd4a79af
jennafrancis <jef399@nyu.edu>
2017-07-18 10:07:45 -0400

    post to auth#login

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -1,4 +1,20 @@
 class Api::AuthController < ApplicationController
   def login
+    @user = User.find_by(email: params [:user][:email])
+    if !@user
+      render json: {
+        errors: {
+          email: ["Unable to find a user with the provided email address"]
+        }
+      }, status: 500
+    elsif @user && @user.authenticate(params[:user][:password])
+      render 'users/user_with_wuth_token.json.jbuilder', user: @user
+    else
+      render json: {
+        errors: {
+          password: ["Password does not match the provided email"]
+        }
+      }, status: 500
+    end
   end
 end
