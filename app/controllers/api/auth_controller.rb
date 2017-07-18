f96f85ab6c7039e8c4594aff9125e6c002eb602e
jennafrancis <jef399@nyu.edu>
2017-07-18 09:56:08 -0400

    Auth controller and login route

---
+++ app/controllers/api/auth_controller.rb
@@ -0,0 +1,4 @@
+class Api::AuthController < ApplicationController
+  def login
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     resources :beers
     resources :users, only: [:create]
+    post '/auth', to: "auth#login"
   end
 end
