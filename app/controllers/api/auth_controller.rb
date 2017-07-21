cdb719a0a98dda2266bd7cc39f41d7a4274bf93b
jennafrancis <jef399@nyu.edu>
2017-07-20 20:08:24 -0400

    change error handling for login auth

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -3,17 +3,13 @@ class Api::AuthController < ApplicationController
     @user = User.find_by(email: params[:user][:email])
     if !@user
       render json: {
-        errors: {
-          email: ["Unable to find a user with the provided email address"]
-        }
+        error: "Unable to find a user with the provided email address"
       }, status: 500
     elsif @user && @user.authenticate(params[:user][:password])
       render 'users/user_with_token.json.jbuilder', user: @user
     else
       render json: {
-        errors: {
-          password: ["Password does not match the provided email"]
-        }
+        error: "Password does not match the provided email"
       }, status: 500
     end
   end
